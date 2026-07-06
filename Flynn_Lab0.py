f78c049c8d8f244407eb7d685337ed7b7c016bc2
addisonflynn87 <addisonflynn87@gmail.com>
2026-07-06 09:09:38 -0500

    Add files via upload

---
+++ Flynn_Lab0.py
@@ -0,0 +1,15 @@
+################################################################################
+#   Purpose:  Create a simple Python program to display personal information.
+#
+#   Author: Addison Flynn
+#
+#   Date Written: 1/20/2026
+#
+#   Version: 1.0.0.0
+#
+################################################################################
+
+
+print('My Name is Addison Flynn')
+print('My major is Cybersecurity')
+print('My favorite movie is Lord of the Ring: The Two Towers')
